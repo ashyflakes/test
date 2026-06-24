186aa88b9d2be9e197cc9876772fada07249a036
ashyflakes <alexandr.cherkaschenko@gmail.com>
2026-06-24 22:51:08 +0300

    added theme saving in index.js

--- index.js
+++ index.js
@@ -2,16 +2,21 @@
 
 const darkModeBtn = document.querySelector(".dark_mode");
 
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "dark") {
+    document.body.classList.add("dark_theme");
+}
+
 darkModeBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark_theme");
 
     if (document.body.classList.contains("dark_theme")) {
+        localStorage.setItem("theme", "dark");
         darkModeBtn.textContent = "🌙";
-        darkModeBtn.style.backgroundColor = "#252525";
-        darkModeBtn.style.border = "solid 2px #f9f9f9"
-    } else {
-        darkModeBtn.textContent = "☀️";
-        darkModeBtn.style.backgroundColor = "#e9e9e9"
-        darkModeBtn.style.border = "solid 2px #101010"
     }
+    else {
+        localStorage.setItem("theme", "light");
+        darkModeBtn.textContent = "☀️";
+    };
 });
